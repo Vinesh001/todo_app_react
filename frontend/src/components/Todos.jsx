284d3a3f2d0df809518062ed3fad5d8bcc676796
Vinesh001 <vineshbaghel10@gmail.com>
2024-01-20 01:36:02 +0530

    frontend part completed as well as it connected to backend the only problem in to stop infinite re-rendering

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+
+
+export function Todos({todos}){
+    return <div>
+        {todos.map(function(todo){
+            return <div>
+                <h1>{todo.title}</h1>
+                <h1>{todo.description}</h1>
+                <button>{todo.completed==true?"completed":"Mark as completed"}</button>
+            </div>
+        })}
+    </div>
+}
